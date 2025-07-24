9db35986976d41c336113f8e96056b5e660db70e
CiabattaCerals <ciabattacereals@gmail.com>
2025-07-24 11:10:37 +0800

    Kotlin to Java

---
+++ app/src/main/java/com/example/pic_4_words_java/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.pic_4_words_java;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.pic_4_words_java.MainMenu.MainMenuFragmentStateAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewPager2 viewPager = findViewById(R.id.viewpager);
+        MainMenuFragmentStateAdapter adapter = new MainMenuFragmentStateAdapter(this);
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/example/pic_4_words_java/MainMenu/MainMenuFragment.java
@@ -0,0 +1,38 @@
+package com.example.pic_4_words_java.MainMenu;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.pic_4_words_java.Game.Game;
+import com.example.pic_4_words_java.R;
+
+public class MainMenuFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_menu, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button startButton = view.findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(requireContext(), Game.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
