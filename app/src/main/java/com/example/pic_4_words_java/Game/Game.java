9c82085f396bd1952485bdfdeca46697b22dd01b
CiabattaCerals <ciabattacereals@gmail.com>
2025-07-28 17:12:44 +0800

    Quiz part somewhat working

--- app/src/main/java/com/example/pic_4_words_java/Game/Game.java
+++ app/src/main/java/com/example/pic_4_words_java/Game/Game.java
@@ -4,12 +4,10 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.pic_4_words_java.Game.Fragments.DifficultyFragment;
 import com.example.pic_4_words_java.R;
 
 public class Game extends AppCompatActivity {
