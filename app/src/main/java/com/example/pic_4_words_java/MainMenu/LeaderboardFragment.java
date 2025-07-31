fb2603a5e96de828d9f3795873fdfa159c3452ce
BatteryScentedCandle <aldeasandwich@gmail.com>
2025-08-01 01:54:57 +0800

    Added a Leaderboard (This is now a leaderboard branch)

--- app/src/main/java/com/example/pic_4_words_java/MainMenu/LeaderboardFragment.java
+++ app/src/main/java/com/example/pic_4_words_java/MainMenu/LeaderboardFragment.java
@@ -5,14 +5,23 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.pic_4_words_java.Game.Fragments.Score;
+import com.example.pic_4_words_java.MainMenu.Leaderboard.LeaderboardAdapter;
+import com.example.pic_4_words_java.MainMenu.Leaderboard.UserScore;
 import com.example.pic_4_words_java.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeaderboardFragment extends Fragment {
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +30,19 @@ public class LeaderboardFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+        View view = inflater.inflate(R.layout.fragment_leaderboard, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+
+        List<UserScore> userScores = new ArrayList<>();
+
+        userScores.add(new UserScore("Squiggly Miggly", 15));
+        userScores.add(new UserScore("Micropachycephalosaurus", 12));
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        LeaderboardAdapter adapter = new LeaderboardAdapter(userScores);
+        recyclerView.setAdapter(adapter);
+        return view;
     }
 }
