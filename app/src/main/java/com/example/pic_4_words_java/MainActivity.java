936bb4dea3d9e79c7ef56f1b7ea67564ce1ca61b
CiabattaCerals <ciabattacereals@gmail.com>
2025-08-03 17:10:59 +0800

    Added Sam's BGM code

--- app/src/main/java/com/example/pic_4_words_java/MainActivity.java
+++ app/src/main/java/com/example/pic_4_words_java/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.pic_4_words_java;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
@@ -16,6 +18,9 @@ import com.example.pic_4_words_java.MainMenu.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    MediaPlayer player;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +30,31 @@ public class MainActivity extends AppCompatActivity {
         ViewPager2 viewPager = findViewById(R.id.viewpager);
         MainMenuFragmentStateAdapter adapter = new MainMenuFragmentStateAdapter(this);
         viewPager.setAdapter(adapter);
+
+        player = MediaPlayer.create(MainActivity.this, R.raw.m_b4);
+        player.setLooping(true);
+        player.setVolume(100, 100);
+        player.start();
     }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        player.stop();
+        player.release();
+    }
+
+
+
+
+    //these functions are not used
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player.start();
+        return startId;
+    }
+
+    public void onStart(Intent intent, int startId) {
+    }
+
 }
